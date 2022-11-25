3477547362e13aacfc8b38f0f40e09ee1e666ce6
Larry Zhang <lzhang124@gmail.com>
2022-11-25 10:05:27 -0600

    more points

--- points.js
+++ points.js
@@ -22,7 +22,7 @@ POINTS_DATA = [
       },
       {
         'name': 'Missing Suit',
-        'description': 'can include word tiles'
+        'description': 'can include words'
       },
       {
         'name': 'Gang',
@@ -43,12 +43,12 @@ POINTS_DATA = [
       },
       {
         'name': '2x Pure Chi',
-        'description': '2x chi, same sequence, same suit',
+        'description': '2x chi, same suit, same sequence',
         'example': ['5g', '6g', '7g', ' ', '5g', '6g', '7g'],
       },
       {
         'name': '2x Mixed Chi',
-        'description': '2x chi, same sequence, different suits',
+        'description': '2x chi, different suits, same sequence',
         'example': ['5g', '6g', '7g', ' ', '5r', '6r', '7r'],
       },
     ],
@@ -119,21 +119,118 @@ POINTS_DATA = [
   {
     'points': 6,
     'hands': [
+      {
+        'name': 'Fully Revealed Hand',
+        'description': 'all sets revealed, won by discard',
+      },
+      {
+        'name': 'All Peng',
+        'description': '4x peng/gang',
+        'example': ['3g', '3g', '3g', ' ', '4r', '4r', '4r', ' ', '8r', '8r', '8r', ' ', 'bb', 'bb', 'bb', ' ', 'wb', 'wb'],
+      },
+      {
+        'name': 'Half Flush',
+        'description': 'all one suit, can include words',
+        'example': ['3g', '3g', '3g', ' ', '4g', '5g', '6g', ' ', '8g', '8g', '8g', ' ', 'bb', 'bb', 'bb', ' ', 'wb', 'wb'],
+      },
+      {
+        'name': '3x Mixed Shifted Chi',
+        'description': '3x chi, different suit, shifted 1',
+        'example': ['4g', '5g', '6g', ' ', '5r', '6r', '7r', ' ', '6b', '7b', '8b'],
+      },
+      {
+        'name': 'All Types',
+        'description': 'all sets of different type (suit, winds, GRB)',
+        'example': ['1g', '2g', '3g', ' ', '5r', '5r', '5r', ' ', '6b', '7b', '8b', ' ', 'bb', 'bb', 'bb', ' ', 'wb', 'wb'],
+      },
+      {
+        'name': '2x Dragon Peng',
+        'description': '2x GRB peng/gang',
+      },
     ],
   },
   {
     'points': 8,
     'hands': [
+      {
+        'name': 'Chicken Hand',
+        'description': 'hand worth 0',
+      },
+      {
+        'name': 'Last Tile',
+        'description': 'self draw or discard',
+      },
+      {
+        'name': 'Gang Replacement',
+        'description': 'replacement tile from gang',
+      },
+      {
+        'name': 'Robbing the Gang',
+        'description': 'tile used to make gang',
+      },
+      {
+        'name': '2x Concealed Gang',
+      },
+      {
+        'name': 'Mixed Straight',
+        'description': '9 straight, different suit',
+        'example': ['1g', '2g', '3g', ' ', '4r', '5r', '6r', ' ', '7b', '8b', '9b'],
+      },
+      {
+        'name': 'Reversible Tiles',
+        'description': 'only 245689 sticks, 1234589 stones, B',
+        'example': ['4g', '5g', '6g', ' ', '8b', '8b', '8b', ' ', '9g', '9g', '9g', ' ', '3b', '4b', '5b', ' ', 'bb', 'bb'],
+      },
+      {
+        'name': '3x Mixed Chi',
+        'description': '3x chi, different suit, same sequence',
+        'example': ['5g', '6g', '7g', ' ', '5r', '6r', '7r', ' ', '5b', '6b', '7b'],
+      },
+      {
+        'name': '3x Mixed Shifted Peng',
+        'description': '3x peng/gang, different suit, shifted 1',
+        'example': ['6g', '6g', '6g', ' ', '7r', '7r', '7r', ' ', '8b', '8b', '8b'],
+      },
     ],
   },
   {
     'points': 12,
     'hands': [
+      {
+        'name': 'Lesser Knitted + Words',
+        'description': 'any words and knitted sequences, different suit',
+        'example': ['1g', '4g', '7g', ' ', '2r', '5r', '8r', ' ', '3b', '9b', ' ', 'W', 'W', 'W', 'G', 'R', 'B'],
+      },
+      {
+        'name': 'Knitted Straight',
+        'description': '3x knitted sequences, different suit',
+        'example': ['1g', '4g', '7g', ' ', '2r', '5r', '8r', ' ', '3b', '6b', '9b'],
+      },
+      {
+        'name': 'Lower 4',
+        'description': 'only 1-4',
+        'example': ['1g', '2g', '3g', ' ', '4g', '4g', '4g', ' ', '3r', '3r', '3r', ' ', '2b', '3b', '4b', ' ', '1b', '1b'],
+      },
+      {
+        'name': 'Upper 4',
+        'description': 'only 6-9',
+        'example': ['6g', '6g', '6g', ' ', '7g', '8g', '9g', ' ', '9r', '9r', '9r', ' ', '6b', '7b', '8b', ' ', '9b', '9b'],
+      },
+      {
+        'name': '3x Winds',
+        'description': '3x wind peng/gang',
+        'example': ['wb', 'wb', 'wb', ' ', 'wb', 'wb', 'wb', ' ', 'wb', 'wb', 'wb'],
+      },
     ],
   },
   {
     'points': 16,
     'hands': [
+      {
+        'name': 'Pure Straight',
+        'description': '9 straight, same suit',
+        'example': ['1g', '2g', '3g', ' ', '4g', '5g', '6g', ' ', '7g', '8g', '9g'],
+      },
     ],
   },
   {
@@ -155,8 +252,8 @@ POINTS_DATA = [
         'example': ['2g', '3g', '4g', ' ', '2g', '3g', '4g', ' ', '2g', '3g', '4g', ' ', '2g', '3g', '4g'],
       },
       {
-        'name': '4x Shifted Peng',
-        'description': '4x peng/gang, same suit, shifted',
+        'name': '4x Pure Shifted Peng',
+        'description': '4x peng/gang, same suit, shifted 1',
         'example': ['2g', '2g', '2g', ' ', '3g', '3g', '3g', ' ', '4g', '4g', '4g', ' ', '5g', '5g', '5g'],
       },
     ],
